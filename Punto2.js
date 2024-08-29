49fb1718a08d83730214c9a8419cd8bff42a8c45
SrWicho13 <luanortegami@ittepic.edu.mx>
2024-08-28 21:11:18 -0700

    punto 2

---
+++ Punto2.js
@@ -0,0 +1,6 @@
+function reversoDeCadena(frase) {
+    return frase.split(' ').reverse().join(' ');
+  }
+  
+  console.log(reversoDeCadena("Hola mundo en JavaScript"));  // Salida: "JavaScript en mundo Hola"
+  
